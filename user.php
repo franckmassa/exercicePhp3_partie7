1d2417d3d69befc9c89510930ca8cc1c14b79af2
massa <massafranck@gmail.com>
2018-08-22 21:53:54 +0200

    corrigé

--- user.php
+++ user.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice3</title>
-</head>
-<body>
-    <!--Récupération des données du formulaire-->
-    Bonjour <?php echo $_GET['lastName'] . ' ' . $_GET['firstName']; ?>
-  
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice3</title>
+    </head>
+    <body>
+        <?php
+        if (!empty($_GET['firstName']) && !empty($_GET['lastName'])) {
+            //Récupération des données du formulaire
+            echo 'Bonjour ' . $_GET['lastName'] . ' ' . $_GET['firstName'];
+        } else {
+            echo "Champs vides";
+        }
+        ?>
+    </body>
 </html>
